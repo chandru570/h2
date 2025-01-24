bc8be96e9433aca6d4d3b5ab22945b2a5d05283a
Chandra kanth <chandru570@gmail.com>
2025-01-24 01:40:44 +0000

    Add jacoco plugin, fix sonarqube violations

--- src/test/java/com/example/h2/TestControllerTest.java
+++ src/test/java/com/example/h2/TestControllerTest.java
@@ -3,8 +3,6 @@ package com.example.h2;
 import com.example.h2.service.TestService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
